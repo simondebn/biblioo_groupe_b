1d4eaeb0ee000ee27b41d7cd0772b48b52dadfdb
Vincent D <doreauvincent13@gmail.com>
2017-12-06 10:12:16 +0100

    Beauté Commentaire

--- app/view/modalComments.php
+++ app/view/modalComments.php
@@ -19,31 +19,39 @@
         <div class="modal-body" style="padding:0 20%; padding-top:20px">
             <div class="row">
 
-                <div class="md-form col-8">
-                    <input type="hidden" id="id_ressource" name="id_ressource" class="form-control" value="">
-                    <input type="text" id="nom" name="nom" class="form-control"><label for="nom" class="">Nom</label>
+                <div class="col-md-6">
+                    <div class="md-form">
+                        <input type="hidden" id="id_ressource" name="id_ressource" class="form-control" value="">
+                        <input type="text" id="nom" name="nom" class="form-control"><label for="nom" class="">Nom</label>
+                    </div>
                 </div>
-                <div class="col-4">
-
-                    <fieldset class="starability-grow">
-                        <input type="radio" id="rate1" name="note" value="1"/>
-                        <label for="rate1" title="Terrible">1 stars</label>
-                        <input type="radio" id="rate2" name="note" value="2"/>
-                        <label for="rate2" title="Not good">2 stars</label>
-                        <input type="radio" id="rate3" name="note" value="3"/>
-                        <label for="rate3" title="Average">3 stars</label>
-                        <input type="radio" id="rate4" name="note" value="4"/>
-                        <label for="rate4" title="Very good">4 stars</label>
-                        <input type="radio" id="rate5" name="note" value="5" checked/>
-                        <label for="rate5" title="Amazing">5 star</label>
-                    </fieldset>
+                <div class="col-md-6">
+                    <div class="md-form">
+                        <fieldset class="starability-grow">
+                            <input type="radio" id="rate1" name="note" value="1"/>
+                            <label for="rate1" title="Terrible">1 stars</label>
+                            <input type="radio" id="rate2" name="note" value="2"/>
+                            <label for="rate2" title="Not good">2 stars</label>
+                            <input type="radio" id="rate3" name="note" value="3"/>
+                            <label for="rate3" title="Average">3 stars</label>
+                            <input type="radio" id="rate4" name="note" value="4"/>
+                            <label for="rate4" title="Very good">4 stars</label>
+                            <input type="radio" id="rate5" name="note" value="5" checked/>
+                            <label for="rate5" title="Amazing">5 star</label>
+                        </fieldset>
+                    </div>
                 </div>
 
             </div>
-            <div class="md-form">
-                <textarea type="text" id="avis" name="avis" class="md-textarea"></textarea>
-                <label for="avis">Commentaire</label>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form">
+                        <textarea type="text" id="avis" name="avis" class="md-textarea"></textarea>
+                        <label for="avis">Commentaire</label>
+                    </div>
+                </div>
             </div>
+
         </div>
         <div class="modal-footer">
             <div class="col-md-12 text-center">
@@ -52,7 +60,7 @@
         </div>
     </form>
     <?php if (!isset($_GET['comments'])): ?>
-        <p>Il n'y a aucun commentaire !</p>
+        <p style="text-align: center;"><br>Il n'y a aucun commentaire !</p>
     <?php else: ?>
         <table class="table">
             <thead>

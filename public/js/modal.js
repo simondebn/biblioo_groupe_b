a0740cf9920a300a294feedd392b810c11416ffa
Eric <cdi@cefimmac31.local>
2017-12-06 11:42:48 +0100

    form ajout livre

--- public/js/modal.js
+++ public/js/modal.js
@@ -8,7 +8,7 @@ $(function() {
         // affiche modale
 
         $.ajax({
-            url: "app/view/formReserver.php",
+            url: "app/view/formAjoutLivre.php",
             data: {
                 id: ticketID
             }
@@ -25,39 +25,77 @@ $(function() {
         $('.modal.form').modal('show');
     });
 
-    $('body').on('click', '#submitReserver', function(){
-        var params = {
-            'id_ressource': $('#id_ressource')[0].value,
-            'nom': $('#nom')[0].value,
-            'prenom': $('#prenom')[0].value,
-            'promo': $('#promo')[0].value
-        };
-        $.ajax({
-            url: "emprunt",
-            type:'POST',
-            data:
-                {
-                    myFunction:'addEmprunt',
-                    myParams: {
-                        params : params
+    /*$('.datepicker').pickadate({
+
+        weekdaysShort: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+        showMonthsShort: true,
+        today: '',
+        clear: 'Clear selection',
+        close: 'Cancel',
+        editable: true,
+        dateFormat: "dd/mm/yy",
+        formatSubmit: 'yyyy/mm/dd'
+        /*$.extend($.fn.pickadate.defaults, {
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        today: 'aujourd\'hui',
+        clear: 'effacer',
+        formatSubmit: 'yyyy/mm/dd'
+        })*/
+    /*});*/
+
+    $('.datepicker').pickadate({
+
+        weekdaysShort: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+        showMonthsShort: true,
+        today: '',
+        clear: 'Clear selection',
+        close: 'Cancel',
+        editable: true,
+        dateFormat: "dd/mm/yy",
+        formatSubmit: 'yyyy/mm/dd'
+        /*$.extend($.fn.pickadate.defaults, {
+        monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        today: 'aujourd\'hui',
+        clear: 'effacer',
+        formatSubmit: 'yyyy/mm/dd'
+        })*/
+        });
+
+        $('body').on('click', '#submitReserver', function(){
+            var params = {
+                'id_ressource': $('#id_ressource')[0].value,
+                'nom': $('#nom')[0].value,
+                'prenom': $('#prenom')[0].value,
+                'promo': $('#promo')[0].value
+            };
+            $.ajax({
+                url: "emprunt",
+                type:'POST',
+                data:
+                    {
+                        myFunction:'addEmprunt',
+                        myParams: {
+                            params : params
+                        }
+                    },
+                success:function(data){
+                    msg = JSON.parse(data);
+                    if(msg.type == 'success'){
+                        $('.modal.form').modal('hide');
+                        console.log(msg.msg);
+                        bootstrapNotify(msg.msg, msg.type);
                     }
-                },
-            success:function(data){
-                msg = JSON.parse(data);
-                if(msg.type == 'success'){
-                    $('.modal.form').modal('hide');
-                    console.log(msg.msg);
-                    bootstrapNotify(msg.msg, msg.type);
-                }
-                else{
+                    else{
 
+                    }
                 }
-            }
-        })
-    });
+            })
+        });
 
-    /** Ajouter les modales pour ajout/suppression/modification des ressources et admins en mode administrateur ainsi que le retour des emprunts
-     * Controllers déjà créés (possiblement à revoir)
-     */
+        /** Ajouter les modales pour ajout/suppression/modification des ressources et admins en mode administrateur ainsi que le retour des emprunts
+         * Controllers déjà créés (possiblement à revoir)
+         */
 
 });
